328e1ff12e1aa16d82e0566044c224327bf86c32
nishita-tuteja <24csu145@ncuindia.edu>
2025-11-16 14:40:33 +0530

    Add files via upload

---
+++ Java Experiment/Experiment 7/excercises/java/DemoPackage.java
@@ -0,0 +1,11 @@
+package exercises.java;
+
+public class DemoPackage {
+    public void display() {
+        System.out.println("PackageDemo executed");
+    }
+
+    public static void main(String[] args) {
+        new DemoPackage().display();
+    }
+}
---
+++ Java Experiment/Experiment 7/excercises/java/DemoPackageDriver.java
@@ -0,0 +1,8 @@
+package exercises.java;
+
+public class DemoPackageDriver {
+    public static void main(String[] args) {
+        DemoPackage obj = new DemoPackage();
+        obj.display();  // Calling display() method
+    }
+}
